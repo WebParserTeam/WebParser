538b8f12c1ec1d6e80ff1bbc7e452ec85b5e9bf8
huangdejun1511425 <1868486175@qq.com>
2018-01-24 22:27:39 +0800

    test

--- parse/urls.py
+++ parse/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import include, url
 from .views import index, reader
-
+#hello
 urlpatterns = [
     url(r'^$', index.show, name = 'index'),
     url(r'^parseWeb/$', reader.parseWeb, name = 'parseWeb'),
